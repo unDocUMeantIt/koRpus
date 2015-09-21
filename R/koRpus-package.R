f31398e0178fd089b0878fa941ed05e6b7314ac2
unDocUMeantIt <m@reaktanz.de>
2015-09-21 17:26:34 +0200

    set new revision for development branch
     - now 0.06-3

--- R/koRpus-package.R
+++ R/koRpus-package.R
@@ -3,7 +3,7 @@
 #' \tabular{ll}{
 #' Package: \tab koRpus\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.06-2\cr
+#' Version: \tab 0.06-3\cr
 #' Date: \tab 2015-09-21\cr
 #' Depends: \tab R (>= 2.10.0),methods\cr
 #' Enhances: \tab rkward\cr
