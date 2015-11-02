9158f08ebca1db820de69ac5efc2faeb884d787c
unDocUMeantIt <m@reaktanz.de>
2015-11-02 11:50:32 +0100

    bump version number
      - new development version (number might change again with actual release)

--- R/koRpus-package.R
+++ R/koRpus-package.R
@@ -3,7 +3,7 @@
 #' \tabular{ll}{
 #' Package: \tab koRpus\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.06-3\cr
+#' Version: \tab 0.06-4\cr
 #' Date: \tab 2015-11-02\cr
 #' Depends: \tab R (>= 2.10.0),methods\cr
 #' Enhances: \tab rkward\cr
