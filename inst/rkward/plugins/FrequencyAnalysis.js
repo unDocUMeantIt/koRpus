0e6768d709959aed1b4d700bb55651bd85713028
unDocUMeantIt <m@reaktanz.de>
2015-11-20 21:13:03 +0100

    optimized RKWard plugin script and added i18n
      - there's now a german translation of the RKWard plugin available

--- inst/rkward/plugins/FrequencyAnalysis.js
+++ inst/rkward/plugins/FrequencyAnalysis.js
@@ -1,75 +1,71 @@
-// this code was generated using the rkwarddev package.
-//perhaps don't make changes here, but in the rkwarddev script instead!
+
 
 
 
 function preprocess(){
-	// add requirements etc. here
+  // add requirements etc. here
 
 }
 
 function calculate(){
-	// read in variables from dialog
+  // read in variables from dialog
+  var varHyphenTagged = getValue("varHyphenTagged");
+  var varkRpFreqObj = getValue("varkRpFreqObj");
+  var tfidf = getValue("tfidf");
+  var freqShowTypes = getValue("freqShowTypes");
+  var saveFrq = getValue("saveFrq");
+  var corpusDB = getValue("corpusDB");
+  var corpDBdir = getValue("corpDBdir");
+  var CelexRunWd = getValue("CelexRunWd");
+  var saveCorpFrq = getValue("saveCorpFrq");
 
-	var varHyphenTagged = getValue("varHyphenTagged");
-	var varkRpFreqObj = getValue("varkRpFreqObj");
-	var chcTrmfrqnc = getValue("chc_Trmfrqnc");
-	var chcShwfrqnc = getValue("chc_Shwfrqnc");
-	var saveFrq = getValue("saveFrq");
-	var drpUscrpsdt = getValue("drp_Uscrpsdt");
-	var brwDatabsfl = getValue("brw_Databsfl");
-	var spnNmbrfrnn = getValue("spn_Nmbrfrnn");
-	var saveCorpFrq = getValue("saveCorpFrq");
-
-	// the R code to be evaluated
-	if(!varkRpFreqObj) {
-		if(drpUscrpsdt == "LCC" && brwDatabsfl) {
-			echo("corp.freq.obj <- read.corp.LCC(\n\t\"" + brwDatabsfl + "\"\n)\n\n");
-		} else if(drpUscrpsdt == "celex" && brwDatabsfl && spnNmbrfrnn) {
-			echo("corp.freq.obj <- read.corp.celex(\n\t\"" + brwDatabsfl + "\",\n\trunning.words=" + spnNmbrfrnn + "\n)\n\n");
-		}
-	}
-	echo("freq.analysis.obj <- freq.analysis(\n\t" + varHyphenTagged);
-	if(!varkRpFreqObj) {
-		if(drpUscrpsdt != "none") {
-			echo(",\n\tcorp.freq=corp.freq.obj");
-		}
-	} else {
-		echo(",\n\tcorp.freq=" + varkRpFreqObj);
-	}
-	if(!chcTrmfrqnc) {
-		echo(",\n\ttfidf=FALSE");
-	}
-	echo("\n)\n\n");
+  // the R code to be evaluated
+  if(!varkRpFreqObj) {
+    if(corpusDB == "LCC" && corpDBdir) {
+      echo("corp.freq.obj <- read.corp.LCC(\n\t\"" + corpDBdir + "\"\n)\n\n");  
+    } else if(corpusDB == "celex" && corpDBdir) {
+      echo("corp.freq.obj <- read.corp.celex(\n\t\"" + corpDBdir + "\",\n\trunning.words=" + CelexRunWd + "\n)\n\n");  
+    } else {}  
+  } else {}
+  echo("freq.analysis.obj <- freq.analysis(\n\t" + varHyphenTagged);
+  if(!varkRpFreqObj) {
+    if(corpusDB != "none") {
+      echo(",\n\tcorp.freq=corp.freq.obj");  
+    } else {}  
+  } else {
+    echo(",\n\tcorp.freq=" + varkRpFreqObj);  
+  }
+  if(!tfidf) {
+    echo(",\n\ttfidf=FALSE");  
+  } else {}
+  echo("\n)\n\n");
 }
 
 function printout(){
-	// printout the results
+  // printout the results
 
 
-	var chcShwfrqnc = getValue("chc_Shwfrqnc");
-	new Header(i18n("Frequency analysis results")).print();
-
-	echo("rk.print(summary(freq.analysis.obj))\n\n");
-	if(chcShwfrqnc) {
-		new Header(i18n("Frequencies of types & token (by word class)"), 3).print();
-
-	echo("freqTypeToken <- data.frame(\n\t" + "types=slot(freq.analysis.obj, \"desc\")$freq.types,\n\t" + "token=slot(freq.analysis.obj, \"desc\")$freq.token\n)\n" + "rk.print(freqTypeToken)\n\n");
-	}
-	//// save result object
-	// read in saveobject variables
-	var saveFrq = getValue("saveFrq");
-	var saveFrqActive = getValue("saveFrq.active");
-	var saveFrqParent = getValue("saveFrq.parent");	var saveCorpFrq = getValue("saveCorpFrq");
-	var saveCorpFrqActive = getValue("saveCorpFrq.active");
-	var saveCorpFrqParent = getValue("saveCorpFrq.parent");
-	// assign object to chosen environment
-	if(saveFrqActive) {
-		echo(".GlobalEnv$" + saveFrq + " <- freq.analysis.obj\n");
-	}
-	if(saveCorpFrqActive) {
-		echo(".GlobalEnv$" + saveCorpFrq + " <- corp.freq.obj\n");
-	}
+  var freqShowTypes = getValue("freqShowTypes");
+  new Header(i18n("Frequency analysis results")).print();
+  echo("rk.print(summary(freq.analysis.obj))\n\n");
+  if(freqShowTypes) {
+    new Header(i18n("Frequencies of types & token (by word class)"), 3).print();  
+    echo("freqTypeToken <- data.frame(\n\t" + "types=slot(freq.analysis.obj, \"desc\")$freq.types,\n\t" + "token=slot(freq.analysis.obj, \"desc\")$freq.token\n)\n" + "rk.print(freqTypeToken)\n\n");  
+  } else {}
+  //// save result object
+  // read in saveobject variables
+  var saveFrq = getValue("saveFrq");
+  var saveFrqActive = getValue("saveFrq.active");
+  var saveFrqParent = getValue("saveFrq.parent");  var saveCorpFrq = getValue("saveCorpFrq");
+  var saveCorpFrqActive = getValue("saveCorpFrq.active");
+  var saveCorpFrqParent = getValue("saveCorpFrq.parent");
+  // assign object to chosen environment
+  if(saveFrqActive) {
+    echo(".GlobalEnv$" + saveFrq + " <- freq.analysis.obj\n");
+  } else {}
+  if(saveCorpFrqActive) {
+    echo(".GlobalEnv$" + saveCorpFrq + " <- corp.freq.obj\n");
+  } else {}
 
 }
 
--- inst/rkward/plugins/Hyphenation.js
+++ inst/rkward/plugins/Hyphenation.js
@@ -1,42 +1,39 @@
-// this code was generated using the rkwarddev package.
-//perhaps don't make changes here, but in the rkwarddev script instead!
+
 
 
 
 function preprocess(){
-	// add requirements etc. here
+  // add requirements etc. here
 
 }
 
 function calculate(){
-	// read in variables from dialog
+  // read in variables from dialog
+  var varHyphenTagged = getString("varHyphenTagged");
+  var saveHyphen = getString("saveHyphen");
+  var showHyphenation = getBoolean("showHyphenation.state");
 
-	var varHyphenTagged = getString("varHyphenTagged");
-	var saveHyphen = getString("saveHyphen");
-	var showHyphenation = getBoolean("showHyphenation.state");
-
-	// the R code to be evaluated
-	echo("hyphenated.text.obj <- hyphen(\n\t" + varHyphenTagged + ",\n\tquiet=TRUE\n)\n\n");
+  // the R code to be evaluated
+  echo("hyphenated.text.obj <- hyphen(\n\t" + varHyphenTagged + ",\n\tquiet=TRUE\n)\n\n");
 }
 
 function printout(){
-	// printout the results
-	new Header(i18n("Hyphenation results")).print();
+  // printout the results
+  new Header(i18n("Hyphenation results")).print();
 
-
-	var showHyphenation = getValue("showHyphenation");
-	if(showHyphenation) {
-		echo("rk.print(hyphenated.text.obj@hyphen)\n\n");
-	}
-	//// save result object
-	// read in saveobject variables
-	var saveHyphen = getValue("saveHyphen");
-	var saveHyphenActive = getValue("saveHyphen.active");
-	var saveHyphenParent = getValue("saveHyphen.parent");
-	// assign object to chosen environment
-	if(saveHyphenActive) {
-		echo(".GlobalEnv$" + saveHyphen + " <- hyphenated.text.obj\n");
-	}
+  var showHyphenation = getValue("showHyphenation");
+  if(showHyphenation) {
+    echo("rk.print(hyphenated.text.obj@hyphen)\n\n");  
+  } else {}
+  //// save result object
+  // read in saveobject variables
+  var saveHyphen = getValue("saveHyphen");
+  var saveHyphenActive = getValue("saveHyphen.active");
+  var saveHyphenParent = getValue("saveHyphen.parent");
+  // assign object to chosen environment
+  if(saveHyphenActive) {
+    echo(".GlobalEnv$" + saveHyphen + " <- hyphenated.text.obj\n");
+  } else {}
 
 }
 
