6dfdc82e0339a47a89708b17cd25d66a95f2f949
unDocUMeantIt <m@reaktanz.de>
2017-06-04 17:03:41 +0200

    fixed show method for new TT.res layout
      - the new factors produced ugly <NA>s and warnings

--- R/02_method_show.kRp.taggedText.R
+++ R/02_method_show.kRp.taggedText.R
@@ -25,14 +25,19 @@
 setMethod("show", signature(object="kRp.taggedText"), function(object){
   txt <- taggedText(object)
   # only print head an tail of long texts
-  if(isTRUE(nrow(txt) > 13)){
+  headLength <- formals(head.matrix)[["n"]]
+  if(isTRUE(nrow(txt) > (headLength * 2 + 1))){
+    # we need to extend the levels of the factors so they don't show ugly <NA>s
+    for (thisFactor in c("tag","desc","wclass","document")){
+      # the dots are actually just needed for "wclass", but we'll keep it simple here...
+      levels(txt[[thisFactor]]) <- c(levels(txt[[thisFactor]]), "", "[...]")
+    }
     # the TT.res slot can change its columns, this must be dealt with dynamically
     middle <- txt[1,]
     for (thisCol in seq_along(middle)){
       # haha, checking for middle class :-D
       middle.class <- class(middle[thisCol])
       middle[1,thisCol] <- switch(middle.class,
-        character="",
         numeric=0,
         logical=NA,
         ""
