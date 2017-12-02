af691b3769da9299e4da47f859ea4b95eef51963
unDocUMeantIt <m@reaktanz.de>
2017-12-02 01:29:07 +0100

    rewrote tests as a first step to remove all internal language support
      - turns out modularizing sylly has more side effects than expected
      - CRAN can build packages which suggest packages from additional repos, but not if they really depend on them
      - consequently, all language support will be outsourced to external packages, and koRpus will not "depend" on but just "suggest" those packages
      - since this would render most unit tests impossible, i've added an internal pseudo language "xyzedish" ("xy")
      - test standards were updated accordingly

--- tests/testthat/test_tokenizing_POS_tagging.R
+++ tests/testthat/test_tokenizing_POS_tagging.R
@@ -1,3 +1,137 @@
+# add support for pseudo language xyzedish to be independent from any actual language support in the package
+# this is basically the english support with a different name
+set.lang.support("treetag",
+  list("xy"=list(
+    ## preset: "xy"
+    lang="xy",
+    encoding="UTF-8",
+    preset=function(TT.cmd, TT.bin, TT.lib, unix.OS){
+      TT.abbrev       <- file.path(TT.lib, "english-abbreviations")
+      TT.lexicon      <- file.path(TT.lib, "english-lexicon.txt")
+      TT.filter       <- "perl -pe 's/\\tV[BDHV]/\\tVB/;s/IN\\/that/\\tIN/;'"
+      TT.lookup       <- file.path(TT.cmd, "lookup.perl")
+      # TT.tokenizer TT.tknz.opts "|" TT.lookup.command TT.tagger TT.opts TT.params TT.filter.command
+      if(isTRUE(unix.OS)){
+        # preset for unix systems
+        return(
+          list(
+            TT.tokenizer      = file.path(TT.cmd, "utf8-tokenize.perl"),
+            TT.tagger         = file.path(TT.bin, "tree-tagger"),
+            TT.abbrev         = TT.abbrev,
+            TT.params         = file.path(TT.lib, "english-utf8.par"),
+            TT.lexicon        = TT.lexicon,
+            TT.lookup         = TT.lookup,
+            TT.filter         = TT.filter,
+
+            TT.tknz.opts      = paste("-e"),
+            TT.lookup.command = paste("perl", TT.lookup, TT.lexicon, "|"),
+            TT.filter.command = paste("|", TT.filter),
+            TT.pre.tagger     = "grep -v '^$' |"
+          )
+        )
+      } else {
+        # preset for windows systems
+        return(
+          list(
+            TT.tokenizer      = file.path(TT.cmd, "utf8-tokenize.perl"),
+            TT.tagger         = file.path(TT.bin, "tree-tagger.exe"),
+            TT.abbrev         = TT.abbrev,
+            TT.params         = file.path(TT.lib, "english-utf8.par"),
+            TT.lexicon        = c(),
+            TT.lookup         = c(),
+            TT.filter         = TT.filter,
+
+            TT.tknz.opts      = paste("-e -a", TT.abbrev),
+            TT.lookup.command = c(),
+            TT.filter.command = paste("|", TT.filter),
+            TT.pre.tagger     = c()
+          )
+        )
+      }
+    })
+  )
+)
+
+set.lang.support("kRp.POS.tags",
+  ## tag and class definitions
+  # xy -- xyzedish
+  # see http://www.ims.uni-stuttgart.de/projekte/corplex/TreeTagger/Penn-Treebank-Tagset.pdf
+  list("xy"=list(
+    tag.class.def.words=matrix(c(
+      "CC", "conjunction", "Coordinating conjunction",
+      "CD", "number", "Cardinal number",
+      "DT", "determiner", "Determiner",
+      "EX", "existential", "Existential there",
+      "FW", "foreign", "Foreign word",
+      "IN", "preposition", "Preposition or subordinating conjunction",
+      "IN/that", "preposition", "Preposition or subordinating conjunction",
+      "JJ", "adjective", "Adjective",
+      "JJR", "adjective", "Adjective, comparative",
+      "JJS", "adjective", "Adjective, superlative",
+      "LS", "listmarker", "List item marker",
+      "MD", "modal", "Modal",
+      "NN", "noun", "Noun, singular or mass",
+      "NNS", "noun", "Noun, plural",
+      "NP", "name", "Proper noun, singular",
+      "NPS", "name", "Proper noun, plural",
+      "NS", "noun", "Noun, plural", # undocumented, bug in parameter file?
+      "PDT", "predeterminer", "Predeterminer",
+      "POS", "possesive", "Possessive ending",
+      "PP", "pronoun", "Personal pronoun",
+      "PP$", "pronoun", "Possessive pronoun",
+      "RB", "adverb", "Adverb",
+      "RBR", "adverb", "Adverb, comparative",
+      "RBS", "adverb", "Adverb, superlative",
+      "RP", "particle", " Particle",
+      "SYM", "symbol", "Symbol",
+      "TO", "to", "to",
+      "UH", "interjection", "Interjection",
+      "VB", "verb", "Verb, base form of \"to be\"",
+      "VBD", "verb", "Verb, past tense of \"to be\"",
+      "VBG", "verb", "Verb, gerund or present participle of \"to be\"",
+      "VBN", "verb", "Verb, past participle of \"to be\"",
+      "VBP", "verb", "Verb, non-3rd person singular present of \"to be\"",
+      "VBZ", "verb", "Verb, 3rd person singular present of \"to be\"",
+      "VH", "verb", "Verb, base form of \"to have\"",
+      "VHD", "verb", "Verb, past tense of \"to have\"",
+      "VHG", "verb", "Verb, gerund or present participle of \"to have\"",
+      "VHN", "verb", "Verb, past participle of \"to have\"",
+      "VHP", "verb", "Verb, non-3rd person singular present of \"to have\"",
+      "VHZ", "verb", "Verb, 3rd person singular present of \"to have\"",
+      "VV", "verb", "Verb, base form",
+      "VVD", "verb", "Verb, past tense",
+      "VVG", "verb", "Verb, gerund or present participle",
+      "VVN", "verb", "Verb, past participle",
+      "VVP", "verb", "Verb, non-3rd person singular present",
+      "VVZ", "verb", "Verb, 3rd person singular present",
+      "WDT", "determiner", "Wh-determiner",
+      "WP", "pronoun", "Wh-pronoun",
+      "WP$", "pronoun", "Possessive wh-pronoun",
+      "WRB", "adverb", "Wh-adverb"
+      ), ncol=3, byrow=TRUE, dimnames=list(c(),c("tag","wclass","desc"))),
+    tag.class.def.punct=matrix(c(
+      ",", "comma", "Comma", # not in guidelines
+      "(", "punctuation", "Opening bracket", # not in guidelines
+      ")", "punctuation", "Closing bracket", # not in guidelines
+      ":", "punctuation", "Punctuation", # not in guidelines
+      "``", "punctuation", "Quote", # not in guidelines
+      "''", "punctuation", "End quote", # not in guidelines
+      "#", "punctuation", "Punctuation", # not in guidelines
+      "$", "punctuation", "Punctuation" # not in guidelines
+      ), ncol=3, byrow=TRUE, dimnames=list(c(),c("tag","wclass","desc"))),
+    tag.class.def.sentc=matrix(c(
+      "SENT", "fullstop", "Sentence ending punctuation" # not in guidelines
+      ), ncol=3, byrow=TRUE, dimnames=list(c(),c("tag","wclass","desc")))
+    )
+  )
+)
+
+# hyph_xy_dput.txt is just a small pattern sample, no more than POC
+# the results are completely useless, except for testing the functionality
+# so don't freak out when you look at them
+samplePatternStandard <- dget("hyph_xy_dput.txt")
+set.hyph.support(list("xy"=samplePatternStandard))
+
 # testing basic tokenizing and POS tagging
 
 context("environment")
@@ -5,14 +139,14 @@ context("environment")
 test_that("setting environment variables", {
   # we cannot really test the treetag function without a local TreeTagger installation,
   # however, we can check if setting the environment works as expected
-  set.kRp.env(TT.cmd="manual", lang="en", TT.options=list(path=".", preset="en"), validate=FALSE)
+  set.kRp.env(TT.cmd="manual", lang="xy", TT.options=list(path=".", preset="xy"), validate=FALSE)
 
   expect_that(get.kRp.env(TT.cmd=TRUE),
     matches("manual"))
   expect_that(get.kRp.env(lang=TRUE),
-    matches("en"))
+    matches("xy"))
   expect_that(get.kRp.env(TT.options=TRUE),
-    is_identical_to(list(path=".", preset="en")))
+    is_identical_to(list(path=".", preset="xy")))
 })
 
 
@@ -28,19 +162,19 @@ test_that("basic tokenizing", {
   # without a local TreeTagger installation, these tests will be limited
   # to what is possible with tokenize()
   tokenizedTextFile <- tokenize(
-    sampleTextFile, lang="en", stopwords=c("it's","one","for","you","and","me"), add.desc=TRUE)
+    sampleTextFile, lang="xy", stopwords=c("it's","one","for","you","and","me"), add.desc=TRUE)
   tokenizedTextFileNoDesc <- tokenize(
-    sampleTextFile, lang="en", stopwords=c("it's","one","for","you","and","me"), add.desc=FALSE)
+    sampleTextFile, lang="xy", stopwords=c("it's","one","for","you","and","me"), add.desc=FALSE)
   tokenizedTextObj <- tokenize(
-    sampleTextObj, format="obj", lang="en", stopwords=c("it's","one","for","you","and","me"), add.desc=TRUE)
+    sampleTextObj, format="obj", lang="xy", stopwords=c("it's","one","for","you","and","me"), add.desc=TRUE)
 
   textToTag <- file(sampleTextFile)
   tokenizedTextConnection <- tokenize(
-    textToTag, lang="en", stopwords=c("it's","one","for","you","and","me"), add.desc=TRUE)
+    textToTag, lang="xy", stopwords=c("it's","one","for","you","and","me"), add.desc=TRUE)
   close(textToTag)
 
   # this was fixed in koRpus 0.06-4, checking it's still working
-  tokenizedToken <- tokenize("singleton", format="obj", lang="en", add.desc=TRUE)
+  tokenizedToken <- tokenize("singleton", format="obj", lang="xy", add.desc=TRUE)
 
   # we can't compare with "is_identical_to() because the percentages may slightly differ
   expect_that(tokenizedTextFile,
@@ -62,7 +196,7 @@ test_that("fixing old objects", {
   )
 
   tokenizedTextFile <- tokenize(
-    sampleTextFile, lang="en", stopwords=c("it's","one","for","you","and","me"), add.desc=TRUE)
+    sampleTextFile, lang="xy", stopwords=c("it's","one","for","you","and","me"), add.desc=TRUE)
 
   # we can't compare with "is_identical_to() because the percentages may slightly differ
   expect_that(tokenizedTextFile,
@@ -96,11 +230,20 @@ test_that("hyphenation/syllable count", {
   sampleTextStandard <- dget("sample_text_hyphen_dput.txt")
   sampleTextStandardChanged <- dget("sample_text_correcthyph_dput.txt")
 
-  hyphenTextObjNoCache <- hyphen(sampleTextTokenized, cache=FALSE, quiet=TRUE)
-  hyphenTextObjCache <- hyphen(sampleTextTokenized, quiet=TRUE)
+  hyphenTextObjNoCache <- hyphen(
+    sampleTextTokenized,
+    hyph.pattern=samplePatternStandard,
+    cache=FALSE,
+    quiet=TRUE
+  )
+  hyphenTextObjCache <- hyphen(
+    sampleTextTokenized,
+    hyph.pattern=samplePatternStandard,
+    quiet=TRUE
+  )
   # chcanging hyphenation
   hyphenTextObjChanged <- correct.hyph(hyphenTextObjCache, "Papua", "Pa-pu-a")
-  hyphenTextObjChanged <- correct.hyph(hyphenTextObjChanged, "al-so", "also")
+  hyphenTextObjChanged <- correct.hyph(hyphenTextObjChanged, "in-edible", "inedible")
 
   expect_that(hyphenTextObjNoCache,
     equals(sampleTextStandard))
