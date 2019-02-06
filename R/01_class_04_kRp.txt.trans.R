77115b897ab824a48424cca5a8b18eb82d5191c9
unDocUMeantIt <m@reaktanz.de>
2019-02-06 16:50:20 +0100

    updated class definition for kRp.txt.trans
      - column names disagreed with what was actually produced by textTransform()

--- R/01_class_04_kRp.txt.trans.R
+++ R/01_class_04_kRp.txt.trans.R
@@ -1,4 +1,4 @@
-# Copyright 2010-2018 Meik Michalke <meik.michalke@hhu.de>
+# Copyright 2010-2019 Meik Michalke <meik.michalke@hhu.de>
 #
 # This file is part of the R package koRpus.
 #
@@ -28,7 +28,7 @@
 #' @slot lang A character string, naming the language that is assumed for the analized text in this object.
 #' @slot desc Descriptive statistics of the tagged text.
 #' @slot TT.res A data.frame with the fully tagged and transformed text (like \code{TT.res} in class \code{koRpus.tagged}, plus
-#'    the new columns \code{token.old} and \code{equal}).
+#'    the new columns \code{token.orig} and \code{equal}).
 #' @slot diff A list with atomic vectors, describing the amount of diffences between both text variants (percentage):
 #'    \describe{
 #'      \item{\code{all.tokens}:}{Percentage of all tokens, including punctuation, that were altered.}
@@ -51,7 +51,7 @@ kRp_txt_trans <- setClass("kRp.txt.trans",
   prototype=prototype(
     lang=character(),
     desc=list(),
-    TT.res=data.frame(token=NA, tag=NA, lemma=NA, lttr=NA, wclass=NA, desc=NA, token.old=NA, equal=NA),
+    TT.res=cbind(init.kRp.tagged.df(), data.frame(token.orig=NA, equal=NA)),
     diff=list()),
   contains=c("kRp.tagged")
 )
@@ -67,12 +67,13 @@ setAs(from="kRp.txt.trans", to="kRp.tagged", function(from){
   }
 )
 
-# setValidity("kRp.analysis", function(object){
-#     TT.res <- object@TT.res
-#     TT.res.names <- dimnames(TT.res)[[2]]
-#     if(identical(TT.res.names, c("word","tag","lemma"))){
-#       return(TRUE)
-#     } else {
-#       stop(simpleError("Invalid object: Wrong column names."))
-#     }
-# })
+setValidity("kRp.txt.trans", function(object){
+  TT.res <- taggedText(object)
+  TT.res.names <- colnames(TT.res)
+  valid.cols <- c(valid.TT.res.kRp.tagged, "token.orig", "equal")
+  if(all(valid.cols %in% TT.res.names)){
+    return(TRUE)
+  } else {
+    stop(simpleError("Invalid object: Wrong column names."))
+  }
+})
--- R/koRpus-package.R
+++ R/koRpus-package.R
@@ -5,7 +5,7 @@
 #' Package: \tab koRpus\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.11-6\cr
-#' Date: \tab 2019-02-05\cr
+#' Date: \tab 2019-02-06\cr
 #' Depends: \tab R (>= 3.0.0),sylly (>= 0.1-4)\cr
 #' Enhances: \tab rkward\cr
 #' Encoding: \tab UTF-8\cr
