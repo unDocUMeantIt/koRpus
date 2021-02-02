892bc6904713d8195de1c0e79401fbbf278d3bea
unDocUMeantIt <meik.michalke@hhu.de>
2021-02-02 14:55:43 +0100

    manually fixed tokenize-methods.Rd and updated for CRAN submission

--- R/koRpus-package.R
+++ R/koRpus-package.R
@@ -5,7 +5,7 @@
 #' Package: \tab koRpus\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.13-5\cr
-#' Date: \tab 2021-02-01\cr
+#' Date: \tab 2021-02-02\cr
 #' Depends: \tab R (>= 3.0.0),sylly (>= 0.1-6)\cr
 #' Enhances: \tab rkward\cr
 #' Encoding: \tab UTF-8\cr
