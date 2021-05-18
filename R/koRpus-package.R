d346fea17a8c0127361510eccb997e61dc861420
unDocUMeantIt <meik.michalke@hhu.de>
2021-05-18 15:19:41 +0200

    bump version number

--- R/koRpus-package.R
+++ R/koRpus-package.R
@@ -4,8 +4,8 @@
 #' \tabular{ll}{
 #' Package: \tab koRpus\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.13-8\cr
-#' Date: \tab 2021-05-17\cr
+#' Version: \tab 0.13-9\cr
+#' Date: \tab 2021-05-18\cr
 #' Depends: \tab R (>= 3.0.0),sylly (>= 0.1-6)\cr
 #' Enhances: \tab rkward\cr
 #' Encoding: \tab UTF-8\cr
